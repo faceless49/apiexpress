627a34bf9a29d236c15e1e22f82aec6b0ce83bdd
faceless49 <digital.zeus@mail.ru>
2024-02-01 17:21:54 +0300

    Маршрутизация

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
+import express from 'express'
+
 const port = 8000;
 
 const app = express();
 
 
 app.all('/hello', (req, res, next) => {
-
   console.log('All');
-
   next();
-
 });
 
-
 const cb = (req, res, next) => {
-
   console.log('CB');
-
   next();
-
 }
 
 app.route('/user')
