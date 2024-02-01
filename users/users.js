851baed09c7bf9ee36fe1aae38bc1c8227d7bf9c
faceless49 <digital.zeus@mail.ru>
2024-02-01 17:34:39 +0300

    Промежуточные обработчики

--- users/users.js
+++ users/users.js
@@ -1,7 +1,10 @@
 import express from 'express';
 
 const userRouter = express.Router();
-
+userRouter.use((req, res, next) => {
+  console.log('Обработчик users');
+  next();
+});
 userRouter.post('/login', (req, res) => {
   res.send('login');
 });
