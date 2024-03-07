1ffe8e3d0901ba0ae37764f7e3d79b611b7edfb9
faceless49 <digital.zeus@mail.ru>
2024-03-07 12:35:41 +0300

    Упражнение - Улучшаем DI

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,22 @@ import { ExeptionFilter } from './error/exeption.filter';
 import { UserController } from './users/users.controller';
 import { LoggerService } from './logger/logger.service';
 import { App } from './app';
-import { Container } from 'inversify';
+import { Container, ContainerModule, interfaces } from 'inversify';
 
-const appContainer = new Container();
-appContainer.bind<ILogger>(TYPES.ILogger).to(LoggerService);
-appContainer.bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
-appContainer.bind<UserController>(TYPES.UserController).to(UserController);
-appContainer.bind<App>(TYPES.Application).to(App);
-const app = appContainer.get<App>(TYPES.Application);
-app.init();
-export { app, appContainer };
+export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
+  bind<ILogger>(TYPES.ILogger).to(LoggerService);
+  bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
+  bind<UserController>(TYPES.UserController).to(UserController);
+  bind<App>(TYPES.Application).to(App);
+});
+
+function bootstrap() {
+  const appContainer = new Container();
+  appContainer.load(appBindings);
+
+  const app = appContainer.get<App>(TYPES.Application);
+  app.init();
+  return { app, appContainer };
+}
+
+export const { app, appContainer } = bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from 'express';
+import { IUserController } from '../users/users.controller.interface';
 import { ILogger } from '../logger/logger.interface';
 import { TYPES } from '../types';
 import { HTTPError } from '../error/http-error.class';
@@ -7,7 +8,7 @@ import { LoggerService } from '../logger/logger.service';
 import { injectable, inject } from 'inversify';
 
 @injectable()
-export class UserController extends BaseController {
+export class UserController extends BaseController implements IUserController {
   constructor(@inject(TYPES.ILogger) private loggerService: ILogger) {
     super(loggerService);
     this.bindRoutes([
